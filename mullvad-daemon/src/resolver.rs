5897d013a13686ad30c599100cd3e3c5d382cb75
Emīls <emils@mullvad.net>
2021-06-03 10:26:06 +0100

    Hopefully we can resolve stuff now

--- mullvad-daemon/src/resolver.rs
+++ mullvad-daemon/src/resolver.rs
@@ -1,11 +1,12 @@
 use trust_dns_client::rr::LowerName;
 use trust_dns_proto::{
-    op::{header::MessageType, op_code::OpCode},
+    op::{header::MessageType, op_code::OpCode, Header},
     rr::domain::Name,
 };
 
 use std::{
     future::Future,
+    str::FromStr,
     sync::{Arc, RwLock},
 };
 
@@ -17,7 +18,11 @@ use tokio1::{
 
 use trust_dns_client::{op::LowerQuery, rr::RecordType};
 use trust_dns_server::{
-    authority::{Catalog, MessageRequest, ZoneType},
+    authority::{
+        AuthLookup, Authority, Catalog, EmptyLookup, LookupObject, MessageRequest,
+        MessageResponseBuilder, ZoneType,
+    },
+    client::rr::dnssec::{Algorithm, SupportedAlgorithms},
     resolver::config::NameServerConfigGroup,
     server::{Request, RequestHandler, ResponseHandler},
     store::forwarder::{ForwardAuthority, ForwardConfig},
@@ -26,27 +31,29 @@ use trust_dns_server::{
 
 
 pub fn start_resolver() {
-    std::thread::spawn(|| {
-        #[cfg(target_os = "macos")]
-        if let Some(gid) = talpid_core::macos::get_exclusion_gid() {
-            let ret = unsafe { libc::setgid(gid) };
-            if ret != 0 {
-                log::error!("Failed to set group ID");
-                return;
-            }
-        } else {
+    std::thread::spawn(run_resolver);
+}
+
+pub fn run_resolver() {
+    #[cfg(target_os = "macos")]
+    if let Some(gid) = talpid_core::macos::get_exclusion_gid() {
+        let ret = unsafe { libc::setgid(gid) };
+        if ret != 0 {
+            log::error!("Failed to set group ID");
             return;
         }
+    } else {
+        return;
+    }
 
-        let rt = Runtime::new().expect("failed to initialize tokio runtime");
-        log::debug!("Running DNS resolver");
-        match rt.block_on(run_resolver()) {
-            Ok(_) => {
-                log::error!("Resolver stopped unexpectedly");
-            }
-            Err(err) => log::error!("Failed to run resolver: {}", err),
+    let rt = Runtime::new().expect("failed to initialize tokio runtime");
+    log::debug!("Running DNS resolver");
+    match rt.block_on(run_resolver_inner()) {
+        Ok(_) => {
+            log::error!("Resolver stopped unexpectedly");
         }
-    });
+        Err(err) => log::error!("Failed to run resolver: {}", err),
+    }
 }
 
 async fn forwarder_authority() -> Result<ForwardAuthority, String> {
@@ -59,22 +66,18 @@ async fn forwarder_authority() -> Result<ForwardAuthority, String> {
 }
 
 struct FilteringHandler {
-    catalog: Catalog,
     allowed_zones: Vec<LowerName>,
     forward_auth: ForwardAuthority,
 }
 
 impl FilteringHandler {
     async fn new() -> Result<Self, String> {
-        let mut catalog = Catalog::new();
-
-        catalog.upsert(
-            LowerName::new(&Name::root()),
-            Box::new(Arc::new(RwLock::new(forwarder_authority().await?))),
-        );
         Ok(Self {
-            catalog,
-            allowed_zones: vec![],
+            allowed_zones: vec![
+                LowerName::from(Name::from_str("ntp.apple.com").unwrap()),
+                LowerName::from(Name::from_str("apple.com").unwrap()),
+                LowerName::from(Name::from_str("www.apple.com").unwrap()),
+            ],
             forward_auth: forwarder_authority().await?,
         })
     }
@@ -95,13 +98,52 @@ impl FilteringHandler {
             && self.allowed_zones.iter().any(|zone| zone == query.name())
     }
 
-    fn lookup(
+    fn lookup<R: ResponseHandler + Unpin>(
         &self,
-        message: &MessageRequest,
-        response_handler: &mut impl ResponseHandler,
+        message: MessageRequest,
+        mut response_handler: R,
     ) -> impl Future<Output = ()> + 'static {
-        async { () }
-        // unimplemented!()
+        async move {
+            let resolver = match forwarder_authority().await {
+                Ok(ok) => ok,
+                Err(err) => {
+                    log::error!("failed to construct a forwarder authority: {}", err);
+                    return;
+                }
+            };
+            for query in message.queries() {
+                let mut response_header = Header::new();
+                response_header.set_id(message.id());
+                response_header.set_op_code(OpCode::Query);
+                response_header.set_message_type(MessageType::Response);
+                response_header.set_authoritative(false);
+                let lookup_result: Box<dyn LookupObject> = resolver
+                    .search(&query, false, SupportedAlgorithms::new())
+                    .await
+                    .map(|lookup| Box::new(lookup) as Box<dyn LookupObject>)
+                    .unwrap_or_else(|err| {
+                        log::error!("error resolving: {}", err);
+                        Box::new(EmptyLookup) as Box<dyn LookupObject>
+                    });
+
+                if !lookup_result.is_empty() {
+                    for addr in lookup_result.iter() {
+                        log::error!("GOT LOOKUP RESULT- {:?}", addr);
+                    }
+                }
+                let response = MessageResponseBuilder::new(Some(message.raw_queries())).build(
+                    response_header,
+                    lookup_result.iter(),
+                    // forwarder responses only contain query answers, no ns,soa or additionals
+                    Box::new(std::iter::empty()) as Box<dyn Iterator<Item = _> + Send>,
+                    Box::new(std::iter::empty()) as Box<dyn Iterator<Item = _> + Send>,
+                    Box::new(std::iter::empty()) as Box<dyn Iterator<Item = _> + Send>,
+                );
+                if let Err(err) = response_handler.send_response(response) {
+                    log::error!("Failed to send response: {}", err);
+                }
+            }
+        }
     }
 
     fn update(&self, message: &MessageRequest, response_handler: &mut impl ResponseHandler) {
@@ -124,14 +166,16 @@ impl RequestHandler for FilteringHandler {
                 }
                 match request.message.op_code() {
                     OpCode::Query => {
-                        return Box::pin(self.lookup(&request.message, &mut response_handle));
+                        return Box::pin(self.lookup(request.message, response_handle));
                     }
                     OpCode::Update => {
                         // TODO: this should be a future
                         self.update(&request.message, &mut response_handle);
                         return Box::pin(async {});
                     }
-                    _ =>  { return Box::pin(async {});  },
+                    _ => {
+                        return Box::pin(async {});
+                    }
                 };
             }
             _ => Box::pin(async {}),
@@ -139,7 +183,7 @@ impl RequestHandler for FilteringHandler {
     }
 }
 
-async fn run_resolver() -> Result<(), String> {
+async fn run_resolver_inner() -> Result<(), String> {
     let mut server_future = ServerFuture::new(FilteringHandler::new().await?);
     let udp_sock = UdpSocket::bind("0.0.0.0:53")
         .await
