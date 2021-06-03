89131587273885d71e89d9a021c8ce1d1eb482a0
Emīls <emils@mullvad.net>
2021-06-03 10:23:47 +0100

    TMP: Add resolver binary

---
+++ mullvad-daemon/src/bin/resolver.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main() {
+    let mut top_dispatcher = fern::Dispatch::new()
+        .level(log::LevelFilter::Debug)
+        .chain(fern::Dispatch::new().chain(io::stdout()));
+    top_dispatcher.apply().expect("failed to init logging");
+    mullvad_daemon::resolver::run_resolver()
+}
