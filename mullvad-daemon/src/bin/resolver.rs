e7aa0d46ec049059ac7ce1c49a852389c51db4eb
Emīls <emils@mullvad.net>
2021-06-10 16:40:34 +0100

    remove dummy resolver

--- mullvad-daemon/src/bin/resolver.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-fn main() {
-    let top_dispatcher = fern::Dispatch::new()
-        .level(log::LevelFilter::Debug)
-        .chain(fern::Dispatch::new().chain(io::stdout()));
-    top_dispatcher.apply().expect("failed to init logging");
-    mullvad_daemon::resolver::run_resolver()
-}
