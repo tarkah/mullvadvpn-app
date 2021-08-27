42dc13748e3bbbc42894fd09c9317170aa15e628
Andrej Mihajlov <and@mullvad.net>
2021-08-27 13:12:57 +0200

    Add promise

---
+++ ios/MullvadVPN/Promise/AnyOptional.swift
@@ -0,0 +1,21 @@
+//
+//  AnyOptional.swift
+//  AnyOptional
+//
+//  Created by pronebird on 22/08/2021.
+//  Copyright © 2021 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+protocol AnyOptional {
+    associatedtype Wrapped
+
+    func asConcreteType() -> Optional<Wrapped>
+}
+
+extension Optional: AnyOptional {
+    func asConcreteType() -> Optional<Wrapped> {
+        return self
+    }
+}
---
+++ ios/MullvadVPN/Promise/AnyResult.swift
@@ -0,0 +1,22 @@
+//
+//  AnyResult.swift
+//  AnyResult
+//
+//  Created by pronebird on 22/08/2021.
+//  Copyright © 2021 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+protocol AnyResult {
+    associatedtype Success
+    associatedtype Failure: Error
+
+    func asConcreteType() -> Result<Success, Failure>
+}
+
+extension Result: AnyResult {
+    func asConcreteType() -> Result<Success, Failure> {
+        return self
+    }
+}
